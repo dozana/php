ffa87c1730994120efde66225809fbaab1305fa9
Levan <levan.soroznishvili@gmail.com>
2023-11-27 15:49:39 +0400

    addcslashes

--- string-functions/addcslashes.php
+++ string-functions/addcslashes.php
@@ -1,5 +1,15 @@
 <?php
 
-echo 'hi';
+/*
+
+you have a string representing a file path, and you want to escape certain characters for security reasons, such as preventing directory traversal attacks.
+
+*/
+
+$filePath = "../uploads/user123/myfile.txt";
+$escapedPath = addcslashes($filePath, '/\\');
+
+echo "Original File Path: $filePath\n";
+echo "Escaped File Path: $escapedPath\n";
 
 ?>
