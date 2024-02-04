dc0ddb1901c982cf76906e7f192e1ba49111738b
Levan <levan@Levans-MacBook-Pro.local>
2024-02-04 19:35:46 +0400

    function parameters

---
+++ for-students/section-4-custom-functions/02-function-parameters/index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+  
+<?php
+
+function message($text) {
+  echo $text . "<br>";
+}
+
+echo message("Hi there");
+echo message("How are you ?");
+
+function calculate($number1, $number2) {
+  $sum = $number1 + $number2 . "<br>";
+  echo $sum;
+}
+
+echo calculate(2, 3);
+echo calculate(4, 6);
+echo calculate(1, 7);
+
+?>
+
+</body>
+</html>
