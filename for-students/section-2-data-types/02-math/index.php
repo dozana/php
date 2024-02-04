e692ec80b83004311799edc4690f92794993d299
Levan <levan@Levans-MacBook-Pro.local>
2024-02-04 17:31:04 +0400

    math

---
+++ for-students/section-2-data-types/02-math/index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+  
+<?php
+
+$a = 2;
+$b = 4;
+$c = 7;
+$d = 1;
+
+echo "$a + $b = " . $a + $b . "<br>";
+echo "$a - $b = " . $a - $b . "<br>";
+echo "$a * $b = " . $a * $b . "<br>";
+echo "$a / $b = " . $a / $b . "<br>";
+echo "$c % $b = " . $c / $b . "<br>";
+
+?>
+
+</body>
+</html>
