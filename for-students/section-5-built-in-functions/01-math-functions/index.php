02a152d3dedd1ae7ca27b26273c1f5a21c706975
Levan <levan.soroznishvili@gmail.com>
2024-02-04 21:55:48 +0400

    math functions

---
+++ for-students/section-5-built-in-functions/01-math-functions/index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+
+<?php
+  echo "pow: " . pow(2, 4);
+  echo "<br>";
+
+  echo "rand: " . rand(1, 1000);
+  echo "<br>";
+
+  echo "sqrt: " . sqrt(100);
+  echo "<br>";
+
+  echo "ceil: " . ceil(4.6);
+  echo "<br>";
+
+  echo "floor: " . floor(4.6);
+  echo "<br>";
+
+  echo "round: " . round(4.5);
+  echo "<br>";
+?>
+
+</body>
+</html>
