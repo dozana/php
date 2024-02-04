fe98b46f9dfb975eeba496590e2bcfc1dba4a015
Levan <levan.soroznishvili@gmail.com>
2024-02-04 23:16:40 +0400

    validate the form values

---
+++ for-students/section-6-form-data/02-validate-the-form-values/index.php
@@ -0,0 +1,59 @@
+<!doctype html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>demo</title>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+</head>
+<body>
+
+<div class="container">
+  <div class="row my-4">
+    <div class="col-md-6">
+
+      <h3 class="mb-3">Input</h3>
+      <form method="post" action="index.php">
+        <div class="mb-3">
+          <label for="username" class="form-label">Username</label>
+          <input type="text" class="form-control" id="username" name="username">
+        </div>
+        <div class="mb-3">
+          <label for="password" class="form-label">Password</label>
+          <input type="password" class="form-control" id="password" name="password">
+        </div>
+        <button type="submit" name="login" class="btn btn-primary btn-sm">Login</button>
+      </form>
+
+    </div>
+    <div class="col-md-6">
+      <h3 class="mb-3">Output</h3>
+      <?php
+        $min = 5;
+        $max = 10;
+        $names = ["john","ann","tom"];
+
+        if ($_SERVER["REQUEST_METHOD"] === "POST") {
+          $username = htmlspecialchars($_POST["username"], ENT_QUOTES, 'UTF-8');
+          $password = htmlspecialchars($_POST["password"], ENT_QUOTES, 'UTF-8');
+      
+          if (empty($username)) {
+              echo "Username is required.";
+          } elseif (strlen($username) < $min || strlen($username) > $max) {
+              echo "Username must be between $min and $max characters.";
+          } elseif (!in_array($username, $name)) {
+              echo "Sorry, you are not allowed.";
+          } else {
+              echo "Welcome!";
+              echo "<p>Hello: <strong>{$username}</strong>, your password is <strong>{$password}</strong></p>";
+          }
+      }
+      ?>
+    </div>
+  </div>
+</div>
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+
+</body>
+</html>
