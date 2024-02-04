fdc662a47480e0bed27a81f0f41fbc6edc718ff5
Levan <levan.soroznishvili@gmail.com>
2024-02-04 22:11:03 +0400

    string functions

---
+++ for-students/section-5-built-in-functions/02-string-functions/index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+  
+<?php
+
+  $message = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas, dignissimos ex ullam vel voluptatum rem.";
+
+  echo strlen($message);
+  echo "<br>";
+
+  echo strtoupper($message);
+  echo "<br>";
+
+  echo strtolower($message);
+  echo "<br>";
+
+?>
+
+</body>
+</html>
