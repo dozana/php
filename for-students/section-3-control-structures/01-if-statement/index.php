33fe2cff96537ce6fe750bdd3d8fe32d6f0ebea2
Levan <levan@Levans-MacBook-Pro.local>
2024-02-04 17:50:47 +0400

    if else statement

---
+++ for-students/section-3-control-structures/01-if-statement/index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+  
+<?php
+
+$age = 18;
+
+if($age < 18) {
+  echo "Age less than 18";
+} elseif($age > 18) {
+  echo "Age more than 18";
+} else {
+  echo "age is 18";
+}
+
+?>
+
+</body>
+</html>
