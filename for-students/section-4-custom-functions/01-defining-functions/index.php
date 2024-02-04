65dea618cf428f43f51bc8a54857cb541402a2f9
Levan <levan@Levans-MacBook-Pro.local>
2024-02-04 19:29:14 +0400

    custom functions

---
+++ for-students/section-4-custom-functions/01-defining-functions/index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+  
+<?php
+
+function sayHi() {
+  echo "Hello <br>";
+}
+
+function calculate() {
+  echo 2+2;
+}
+
+function output() {
+  sayHi();
+  calculate();
+}
+
+output();
+
+?>
+
+</body>
+</html>
