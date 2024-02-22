ddf0088c5369ec4816118c810f7e3eab9b311c4e
Levan <levan.soroznishvili@gmail.com>
2024-02-23 00:05:30 +0400

    database helper functions

--- for-students/section-1-22-authentication/auth-system/functions/db.php
+++ for-students/section-1-22-authentication/auth-system/functions/db.php
@@ -1,3 +1,25 @@
 <?php
 $conn = mysqli_connect("localhost","root","","my_auth2");
+
+function escape($string) {
+  global $conn;
+  mysqli_real_escape_string($conn, $string);
+}
+
+function query($query) {
+  global $conn;
+  return mysqli_query($conn, $query);
+}
+
+function confirm($result) {
+  global $conn;
+  if(!$result) {
+    die("Query Failed: ". mysqli_error($conn));
+  }
+}
+
+function fetch_array($result) {
+  global $conn;
+  return mysqli_fetch_array($result);
+}
 ?>
